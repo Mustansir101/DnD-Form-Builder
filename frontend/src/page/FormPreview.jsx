67acb6f7709557e8f03a0902d68f3550f90e9c14
Mustansir101 <agasi.mustansir@gmail.com>
2025-08-10 22:39:49 +0530

    back to builder

--- frontend/src/page/FormPreview.jsx
+++ frontend/src/page/FormPreview.jsx
@@ -178,7 +178,7 @@ function FormPreview() {
               Take Again
             </button>
             <a
-              href="/"
+              href="/Form-Builder"
               className="bg-gray-600 text-white px-4 py-2 rounded hover:bg-gray-700 inline-block"
             >
               Back to Builder
